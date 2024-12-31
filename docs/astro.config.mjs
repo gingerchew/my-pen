2a76f32ecf6b510a81f530089dacbc76084333a7
ginger <26461046+gingerchew@users.noreply.github.com>
2024-12-31 14:03:39 -0500

    Linting stuff

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,17 +4,15 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://gingerchew.github.io/",
-	base: import.meta.env.DEV ? "" : "/code-cage",
+  site: "https://gingerchew.github.io/",
+  base: import.meta.env.DEV ? "" : "/code-cage",
   integrations: [
     starlight({
-		
       title: "CodeCage",
       social: {
         github: "https://github.com/gingerchew/code-cage",
       },
-      sidebar: [
-      ],
+      sidebar: [],
     }),
   ],
 });
